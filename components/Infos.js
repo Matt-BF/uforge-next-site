288d7007a6532a9905eca7e9fda702dfc654ea0d
Matt <mbfiamenghi@gmail.com>
2021-05-19 19:34:03 -0300

    added fontawesome i tag to title of card

--- components/Infos.js
+++ components/Infos.js
@@ -6,9 +6,9 @@ const Infos = () => {
         <div className={`${stylesInfos.container} ${stylesInfos.flex} bg-secondary`}>
             <h1 style={{"alignSelf":"center"}}>Soluções para seu laboratório</h1>
             <div className={`${stylesInfos.flexGrid}`}>
-            <Card title='Lorem' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
-            <Card title='Lorem' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
-            <Card title='Lorem' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
+            <Card title={<i class="fas fa-flask icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
+            <Card title={<i class="fas fa-graduation-cap icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
+            <Card title={<i class="far fa-comments icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.'/>
             </div>
             
         </div>
