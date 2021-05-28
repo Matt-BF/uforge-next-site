c41ef0a80cc9a6999a58629cd76454df08b571b9
Matt <mbfiamenghi@gmail.com>
2021-05-28 12:51:28 -0300

    started remaking product page

--- data.js
+++ data.js
@@ -1,60 +1,63 @@
 export const products = [
-    {
-        "id": "1",
-        "title": "Kit Sabiá de Beads Magnéticas",
-        "img": "logo_foxes_peq.png",
-        "excerpt": "Lorem ipsum beads sabia",
-        "description": "Lorem lalalallala",
-        "subproducts": [{
-            "id": "1",
-            "title": "Kit Sabiá Leveduras",
-            "description": "levedura"
-        },
-        {
-            "id": "2",
-            "title": "Kit Sabiá Amostras Limpas",
-            "description": "Amostra limpas"
-        }]
-    },
-    {
-        "id": "2",
-        "title": "Ladder",
-        "img": "",
-        "excerpt": "texto curtinho",
-        "description": "Lorem ipsum ladder",
-        "subproducts": [{
-            "id": "1",
-            "title": "Ladder 100bp",
-            "description": "100"
-        }]
-    },
-    {
-        "id": "3",
-        "title": "Racks Magnéticas",
-        "img": "",
-        "excerpt": "Racks magnéticas top da galáxia",
-        "description": "",
-        "subproducts": []
-    }
-]
+  {
+    id: "1",
+    title: "Diagnósticos Clínicos",
+    excerpt: "Lorem ipsum beads sabia",
+    description: "Lorem lalalallala",
+    subproducts: [
+      {
+        id: "1",
+        title: "Kit Sabiá Leveduras",
+        description: "levedura",
+      },
+      {
+        id: "2",
+        title: "Kit Sabiá Amostras Limpas",
+        description: "Amostra limpas",
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "Ladder",
+    img: "",
+    excerpt: "texto curtinho",
+    description: "Lorem ipsum ladder",
+    subproducts: [
+      {
+        id: "1",
+        title: "Ladder 100bp",
+        description: "100",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "Racks Magnéticas",
+    img: "",
+    excerpt: "Racks magnéticas top da galáxia",
+    description: "",
+    subproducts: [],
+  },
+];
 
 export const allNews = [
-    {
-        "id": "1",
-        "backgroundImg": "",
-        "title": "Hello",
-        "text": " "
-    },
-    {
-        "id": "2",
-        "backgroundImg": "",
-        "title": "Second",
-        "text": "Lalala"
-    },
-    {
-        "id": "3",
-        "backgroundImg": "",
-        "title": "Thierry",
-        "text": "Cu de frango"
-    },
-]
+  {
+    id: "1",
+    backgroundImg: "",
+    title: "Hello",
+    text: " ",
+  },
+  {
+    id: "2",
+    backgroundImg: "",
+    title: "Second",
+    text: "Lalala",
+  },
+  {
+    id: "3",
+    backgroundImg: "",
+    title: "Thierry",
+    text: "Cu de frango",
+  },
+];
