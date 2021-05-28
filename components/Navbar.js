5976445c1e2b94e8b27e38e5278fe39c06437083
Matt <mbfiamenghi@gmail.com>
2021-05-28 16:10:33 -0300

    added education page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,31 @@
-import stylesNavbar from '../styles/Navbar.module.css'
-import Link from 'next/link'
-
+import stylesNavbar from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <>
-            <div className={`${stylesNavbar.navbar} ${stylesNavbar.flex} bg-primary`}>
-                <Link href='/'>
-                    <a className={stylesNavbar.imgLink}>
-                        <img src="uforge_full_navbar.png" alt="uForge">
-                        </img>
-                    </a>
-                </Link>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link href='/products'>Produtos</Link>
-                        </li>
-                        <li><a href="#">Educação</a></li>
-                    </ul>
-                </nav>
-            </div>
+  return (
+    <>
+      <div className={`${stylesNavbar.navbar} ${stylesNavbar.flex} bg-primary`}>
+        <Link href="/">
+          <a className={stylesNavbar.imgLink}>
+            <img src="uforge_full_navbar.png" alt="uForge"></img>
+          </a>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/products">Produtos</Link>
+            </li>
+            <li>
+              <Link href="/education">Educação</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
