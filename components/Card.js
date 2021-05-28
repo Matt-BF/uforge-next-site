dd6fa0876b61fcf6829a640eae9f1357016185a2
Matt <mbfiamenghi@gmail.com>
2021-05-28 12:41:27 -0300

    added some text and style changes

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,15 @@
-import stylesCard from '../styles/Card.module.css'
+import stylesCard from "../styles/Card.module.css";
 
-const Card = ({title, text}) => {
-    return (
-        <div className={stylesCard.card}>
-            <h2 className={stylesCard.cardTitle}>{title}</h2>
-            <p className={stylesCard.cardText}>{text}</p>
-        </div>
-    )
-}
+const Card = ({ title, subtitle, text }) => {
+  return (
+    <div className={stylesCard.card}>
+      <h2 className={stylesCard.cardTitle}>{title}</h2>
+      <h3 className={stylesCard.cardSubtitle}>
+        <strong>{subtitle}</strong>
+      </h3>
+      <p className={stylesCard.cardText}>{text}</p>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- components/Infos.js
+++ components/Infos.js
@@ -1,18 +1,39 @@
-import stylesInfos from '../styles/Infos.module.css'
-import Card from './Card'
+import stylesInfos from "../styles/Infos.module.css";
+import Card from "./Card";
 
 const Infos = () => {
-    return (
-        <div className={`${stylesInfos.container} ${stylesInfos.flex} bg-secondary`}>
-            <h1 style={{ "textAlign": "center" }}>Soluções para seu laboratório</h1>
-            <div className={`${stylesInfos.flexGrid}`}>
-                <Card title={<i className="fas fa-flask icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.' />
-                <Card title={<i className="fas fa-graduation-cap icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.' />
-                <Card title={<i className="far fa-comments icon"></i>} text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus.' />
-            </div>
+  return (
+    <div
+      className={`${stylesInfos.container} ${stylesInfos.flex} bg-secondary`}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        <strong>Nosso propósito</strong>
+      </h1>
+      <h2 style={{ textAlign: "center", marginBottom: "40px" }}>
+        Tornar a ciência prazerosa para quem faz, encantadora para quem vive e
+        biorreverente
+      </h2>
+      <div className={`${stylesInfos.flexGrid}`}>
+        <Card
+          title={<i className="fas fa-flask icon"></i>}
+          subtitle="Prazerosa para quem faz"
+          text="Como doutorandos e ex-alunos da UNICAMP, sabemos em primeira
+          mão as dificuldades dos nossos cientistas com produtos que não atendem
+          as demandas ou as expectativas"
+        />
+        <Card
+          title={<i className="fas fa-users icon"></i>}
+          subtitle="Encantadora para quem vive"
+          text=""
+        />
+        <Card
+          title={<i className="fas fa-seedling icon"></i>}
+          subtitle="Biorreverente"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt porro illum, asperiores omnis, molestiae tempora mollitia eaque labore laudantium sit quae error corporis. Eveniet culpa eos porro tenetur natus."
+        />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Infos
+export default Infos;
