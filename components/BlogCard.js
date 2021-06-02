69358ac183912ec8ac337e8c0f52bf7056b25b74
Matt_BF <mbfiamenghi@gmail.com>
2021-06-02 14:24:52 -0300

    styled card

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,12 +3,12 @@ import stylesCard from "../styles/BlogCard.module.css";
 const BlogCard = ({ title, link, date }) => {
   return (
     <div>
-      <a target="_blank" rel="noreferrer" href={link}>
-        <div className={stylesCard.card}>
+      <div className={stylesCard.card}>
+        <a target="_blank" rel="noreferrer" href={link}>
           <h2 className={stylesCard.cardTitle}>{title}</h2>
           <p className={stylesCard.cardText}>Publicado em {date}</p>
-        </div>
-      </a>
+        </a>
+      </div>
     </div>
   );
 };
--- pages/blog.js
+++ pages/blog.js
@@ -35,7 +35,7 @@ const blog = ({ mediumPosts }) => {
 
 export const getServerSideProps = async () => {
   const res = await fetch(
-    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@mattfiamenghi"
+    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@microforge"
   );
   const mediumPosts = await res.json();
   return {
