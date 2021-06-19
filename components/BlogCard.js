9e1b5ded02f0257cd30d6277f00a181ebb24b60e
Matt <mbfiamenghi@gmail.com>
2021-06-19 18:09:35 -0300

    make blog card link black

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,7 +3,12 @@ import stylesCard from "../styles/BlogCard.module.css";
 const BlogCard = ({ title, link, date }) => {
   return (
     <div className={stylesCard.card}>
-      <a target="_blank" rel="noreferrer" href={link}>
+      <a
+        className={stylesCard.link}
+        target="_blank"
+        rel="noreferrer"
+        href={link}
+      >
         <h2 className={stylesCard.cardTitle}>{title}</h2>
         <p className={stylesCard.cardText}>Publicado em {date}</p>
       </a>
