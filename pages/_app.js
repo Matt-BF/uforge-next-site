f792219f0e1e1afd697767cab462d3595c6ea16c
Matt_BF <mbfiamenghi@gmail.com>
2021-12-27 14:49:30 -0300

    disable aos to see

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       once: false, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
-      disable: "mobile",
+      disable: true,
     });
     AOS.refresh();
   }, []);
